9dc8b14ec7467c64e8c01e1b06f386e86f081759
Aaron Luna <admin@aaronluna.dev>
2020-11-19 11:57:06 -0800

    refactored blog_posts.py a bit

--- blog_posts.py
+++ blog_posts.py
@@ -1,20 +1,27 @@
 from feedparser import parse
 
-feed = parse("https://aaronluna.dev/blog/index.xml").entries
+BLOG_URL = "https://aaronluna.dev/blog/"
+BLOG_RSS = f"{BLOG_URL}index.xml"
+README_COMMENT = "<!--blog_posts-->"
+
+feed = parse(BLOG_RSS).entries
 post_list = [
-    f"""- [{feed[i].title}]({feed[i].link})  \n**{feed[i].published}** &mdash; {feed[i].description}\n"""
-    for i in range(3)]
+    f"""- [{f.title}]({f.link})  \n**{f.published}** &mdash; {f.description}\n"""
+    for num, f
+    in enumerate(feed, start=1)
+    if num <= 3
+]
 
 readme_lines = []
 with open("README.md", "r", encoding='utf8') as rd:
     for line in rd:
-        if "<!--blog_posts-->" not in line:
+        if README_COMMENT not in line:
             readme_lines.append(line)
             continue
         break
 
 with open("README.md", "w", encoding='utf8') as rd:
     rd.writelines(readme_lines)
-    rd.write("<!--blog_posts-->\n")
-    rd.write("#### [Most Recent Blog Posts](https://aaronluna.dev/blog/)\n")
-    rd.writelines("\n".join(post_list))
+    rd.write(f"{README_COMMENT}\n")
+    rd.write("## [Most Recent Blog Posts](BLOG_URL)\n")
+    rd.write("\n".join(post_list))
