c99f7a47b13489fc045fd62e82dcfcd8621d5065
Aaron Luna <admin@aaronluna.dev>
2020-11-19 08:09:59 -0800

    minor tweaks to the markdown generated for recent blog posts

--- blog_posts.py
+++ blog_posts.py
@@ -2,18 +2,19 @@ from feedparser import parse
 
 feed = parse("https://aaronluna.dev/blog/index.xml").entries
 post_list = [
-    f"""- [{feed[i].title}]({feed[i].link})  \n{feed[i].description} - {feed[i].published}"""
+    f"""- [{feed[i].title}]({feed[i].link})\n({feed[i].published}) {feed[i].description}\n"""
     for i in range(4)]
 
 readme_lines = []
 with open("README.md", "r", encoding='utf8') as rd:
     for line in rd:
-        if line.strip() == "<!--bp-->":
-            break
-        readme_lines.append(line)
+        if "<!--blog_posts-->" not in line:
+            readme_lines.append(line)
+            continue
+        break
 
 with open("README.md", "w", encoding='utf8') as rd:
     rd.writelines(readme_lines)
-    rd.write("<!--bp-->\n")
-    rd.write("### Most Recent Blog Posts\n")
+    rd.write("<!--blog_posts-->\n")
+    rd.write("#### [Most Recent Blog Posts](https://aaronluna.dev/blog/)\n")
     rd.writelines("\n".join(post_list))
