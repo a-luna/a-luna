0dcbb34b1e9799fc6dc2483138fa9f1cec827f80
a-luna <contact@aaronluna.dev>
2020-12-04 21:34:50 +0000

    made blog_posts.py as generic as possible
    - previous version assumed that rss feed ordered posts by date, descending.
    - now, posts parsed from rss feed are sorted using the published attr.
    - previous version assumed that rss feed contained at least the number of recent posts to display.
    - now, if the number of posts parsed from rss feed is less than the number to display, no error occurs.
    - refactored format_blog_post_as_markdown function out of get_recent_posts function.
    - renamed a bunch of things to be more expressive.

--- blog_posts.py
+++ blog_posts.py
@@ -2,37 +2,43 @@ from feedparser import parse
 
 BLOG_URL = "https://aaronluna.dev/blog/"
 BLOG_RSS = f"{BLOG_URL}index.xml"
-README_COMMENT = "<!--blog_posts-->"
+RECENT_POSTS_BOUNDARY = "<!--blog_posts-->"
 
 
 def update_recent_blog_posts(post_count=3):
     readme_content = get_readme_content()
-    post_list = get_recent_posts(post_count)
-    with open("README.md", "w", encoding='utf8') as readme:
+    recent_posts = get_recent_posts(post_count)
+    with open("README.md", "w", encoding="utf8") as readme:
         readme.writelines(readme_content)
-        readme.write(f"{README_COMMENT}\n")
+        readme.write(f"{RECENT_POSTS_BOUNDARY}\n")
         readme.write(f"## [Recent Blog Posts]({BLOG_URL})\n")
-        readme.write("\n".join(post_list))
+        readme.write("\n".join(recent_posts))
+
 
 def get_readme_content():
     readme_lines = []
-    with open("README.md", "r", encoding='utf8') as readme:
+    with open("README.md", "r", encoding="utf8") as readme:
         for line in readme:
-            if README_COMMENT not in line:
+            if RECENT_POSTS_BOUNDARY not in line:
                 readme_lines.append(line)
                 continue
             break
     return readme_lines
 
+
 def get_recent_posts(post_count):
-    all_blog_posts = parse(BLOG_RSS).entries
-    post_count = len(all_blog_posts) if len(all_blog_posts) < post_count else post_count
-    return [format_blog_post_as_markdown(post) for post in all_blog_posts[:post_count]]
+    all_posts = parse(BLOG_RSS).entries
+    all_posts.sort(key=lambda x: x.published, reverse=True)
+    post_count = post_count if len(all_posts) >= post_count else len(all_posts)
+    return [format_blog_post_as_markdown(post) for post in all_posts[:post_count]]
+
 
 def format_blog_post_as_markdown(post):
     return (
         f"- [{post.title}]({post.link})  \n"
         f"**{post.published}** &mdash; {post.description}\n"
     )
+
+
 if __name__ == "__main__":
     update_recent_blog_posts()
